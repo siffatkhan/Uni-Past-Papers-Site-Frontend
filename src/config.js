4238ae7095be13ed39ed7216accdb3aa4b5ae54b
Siffat Ullah <siffatullah.321@gmail.com>
2026-08-31 06:16:37 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = "http://127.0.0.1:8000";
+// export const API_BASE_URL = "http://127.0.0.1:8000";
+export const API_BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 
 export const JOURNALS_API = `${API_BASE_URL}/journals/api`;
 export const BOOKS_API = `${API_BASE_URL}/books/api`;
